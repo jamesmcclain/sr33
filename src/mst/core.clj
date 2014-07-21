bb82b8dcae509e2939e7164d3de7981902ab1781
James W. McClain <jmcclain@daystrom-data-concepts.com>
2014-07-21 01:51:07 +0000

    Use doseq instead of loop/recur.

--- src/mst/core.clj
+++ src/mst/core.clj
@@ -2,9 +2,6 @@
   (:require [quil.core :as q]
             [clojure.set :as set]))
 
-(defn dist [x y]
-  (Math/sqrt (reduce + (map #(* % %) (map - x y)))))
-
 (defn prim [hood]
   (let [n (count hood)
         uncovered (set (range n))
@@ -15,22 +12,20 @@
                  (>= i n) edges))
         decorate (fn [e]
                    (let [[a b] (vec e)
-                         d (dist (hood a) (hood b))]
+                         d (apply q/dist (concat (hood a) (hood b)))]
                      (vector d e)))
-        edges (sort #(< (first %1) (first %2)) (map decorate edges))
-        edges (map second edges)
-        tree (loop [uncovered (set/difference uncovered (first edges)) tree (vector (first edges))]
-               (if (empty? uncovered)
+        edges (map second (sort #(< (first %1) (first %2)) (map decorate edges)))]
+    (loop [uncovered (set/difference uncovered (first edges)) tree (vector (first edges))]
+      (if (empty? uncovered)
                                         ; if done, return tree
-                 tree
+        (map vec tree)
                                         ; otherwise, build tree
-                 (let [edge (first (filter #(= 1 (count (set/intersection uncovered %))) edges))]
-                   (recur (set/difference uncovered edge) (conj tree edge)))))]
-    (map vec tree)))
+        (let [edge (first (filter #(= 1 (count (set/intersection uncovered %))) edges))]
+          (recur (set/difference uncovered edge) (conj tree edge)))))))
 
 (defn donut? [x]
-  (let [d (dist x [0.5 0.5])]
-    (and (< d 0.5) (> d 0.4))))
+  (let [d (apply q/dist 0.5 0.5 x)]
+    (and (< d 0.50) (> d 0.33))))
 
 (defn generate-point []
   (loop [pt [(rand) (rand)]]
@@ -63,24 +58,17 @@
   (q/background 255)
   
   (let [hood (generate-hood 100)]
-                                        ; lines
-    (q/stroke 255 0 0)
-    (q/stroke-weight 7)
-    (loop [tree (map #(concat (hood (first %)) (hood (second %))) (prim hood))]
-      (if (not (empty? tree))
-        (do
-          (show-line (first tree))
-          (recur (rest tree)))))        ; should be able to map, but can't
-                                        ; points
-    (q/stroke 0)
+    (q/stroke 255 0 0) ; lines
+    (q/stroke-weight 3)
+    (doseq [edge (map #(concat (hood (first %)) (hood (second %))) (prim hood))]
+      (show-line edge))
+    (q/stroke 0)       ; points
     (q/fill 0)
-    (loop [hood hood]
-      (if (not (empty? hood))
-        (do
-          (show-point (first hood))
-          (recur (rest hood)))))))
+    (q/stroke-weight 3)
+    (doseq [point hood]
+      (show-point point))))
 
 (q/defsketch example
   :title "MST"
   :setup setup
-  :size [800 600])
+  :size [500 500])
