52630f418854e98d1c0e9a4c418e460a3c039a4d
James W. McClain <jmcclain@daystrom-data-concepts.com>
2014-07-28 03:55:19 +0000

    Animated MST.
    Partially reconstruct surface data using the MST and show an animated
    representation.

--- src/mst/core.clj
+++ src/mst/core.clj
@@ -4,39 +4,76 @@
   (:use [mst.prim]
         [mst.show]))
 
-(defn donut? [x]
-  (let [d (apply q/dist 0.5 0.5 x)]
-    (and (< d 0.50) (> d 0.33))))
+(def *state* {:points (atom nil) :mst (atom nil) :nn (atom nil)
+              :projection (atom nil) :filename (atom "")})
+(def *angle* (atom 0))
 
-(defn generate-point []
-  (loop [pt [(rand) (rand)]]
-    (if (not (donut? pt))
-      (recur [(rand) (rand)])
-      pt)))
+(defn load-data
+  ([] (load-data "resources/cone.data"))
+  ([filename]
+     (let [points (load-file filename)
+           xmin (reduce min (map #(nth % 0) points))
+           xmax (reduce max (map #(nth % 0) points))
+           xrange (- xmax xmin)
+           ymin (reduce min (map #(nth % 1) points))
+           ymax (reduce max (map #(nth % 1) points))
+           yrange (- ymax ymin)
+           zmin (reduce min (map #(nth % 2) points))
+           zmax (reduce max (map #(nth % 2) points))
+           zrange (- zmax zmin)]
+       (letfn [(squeeze [[a b c]]
+                 [(/ (- a xmin) xrange) (/ (- ymax b) yrange) (/ (- c zmin) zrange)])
+               (projection [[a b c]]
+                 (let [angle (/ @*angle* 2)
+                       [a b c] (vec (map #(- % 0.5) [a b c]))
+                       [a b c] [(+ (* a (Math/cos angle)) (* c (Math/sin angle)))
+                                b
+                                (- (* c (Math/cos angle)) (* a (Math/sin angle)))]
+                       [a b c] [a
+                                (- (* b (Math/cos @*angle*)) (* c (Math/sin @*angle*)))
+                                (+ (* b (Math/sin @*angle*)) (* c (Math/cos @*angle*)))]
+                       [a b c] (vec (map #(+ % 0.5) [a b c]))]
+                   [a b]))]
+         (let [mst (prim points)
+               nn (remove (set mst) (nn-1 points))]
+           (reset! (*state* :points) (map squeeze points))
+           (reset! (*state* :mst) mst)
+           (reset! (*state* :nn) nn)
+           (reset! (*state* :projection) projection)
+           (reset! (*state* :filename) filename))))))
 
-(defn generate-hood [n]
-  (loop [i n hood []]
-    (if (not (zero? i))
-      (recur (dec i) (conj hood (generate-point)))
-      hood)))
+(if (empty? @(*state* :points))
+  (load-data))
 
 (defn setup []
-  (q/smooth)
   (q/frame-rate 7.83)
+  (q/background 255))
+
+(defn draw []
   (q/background 255)
-  
-  (let [hood (generate-hood 100)]
-    (q/stroke 255 0 0) ; lines
-    (q/stroke-weight 3)
-    (doseq [edge (map #(concat (hood (first %)) (hood (second %))) (prim hood))]
+  (q/stroke-weight 3)
+  (swap! *angle* #(mod (+ % 0.01) (* 4 Math/PI)))
+  (let [hood (map @(*state* :projection) @(*state* :points))
+        mst @(*state* :mst)
+        nn @(*state* :nn)]
+
+    ;; (q/stroke-weight 3) ; lines
+    (q/stroke 0 0 255)
+    (doseq [edge (map #(concat (nth hood (first %)) (nth hood (second %))) nn)]
       (show-line edge))
-    (q/stroke 0)       ; points
+    (q/stroke 255 0 0) ; lines
+    (doseq [edge (map #(concat (nth hood (first %)) (nth hood (second %))) mst)]
+      (show-line edge))
+
+    ;; (q/stroke-weight 3) ; points
+    (q/stroke 0)
     (q/fill 0)
-    (q/stroke-weight 3)
     (doseq [point hood]
       (show-point point))))
 
 (q/defsketch example
   :title "MST"
   :setup setup
-  :size [500 500])
+  :draw draw
+  :size [500 500]
+  :renderer :opengl)
