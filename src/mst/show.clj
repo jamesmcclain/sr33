28f4e71c3d3a6b3e795ce7b80728849a10f91500
James W. McClain <jmcclain@daystrom-data-concepts.com>
2014-08-01 23:29:42 +0000

    Move display stuff.

--- src/mst/show.clj
+++ src/mst/show.clj
@@ -1,6 +1,10 @@
 (ns mst.show
   (:require [quil.core :as q]))
 
+(defn setup []
+  (q/frame-rate 20)
+  (q/background 255))
+
 (defn show-point [[x y]]
   (let [diam 10
         x (* x (q/width))
@@ -13,3 +17,33 @@
         y1 (* y1 (q/height))
         y2 (* y2 (q/height))]
     (q/line [x1 y1] [x2 y2])))
+
+(defn draw []
+  (q/background 255)
+  (q/stroke-weight 3)
+  (swap! mst.core/*angle* #(mod (+ % 0.05) (* 4 Math/PI)))
+  (let [hood (map @(mst.core/*state* :projection) @(mst.core/*state* :squeezed-points))
+        mst @(mst.core/*state* :mst)
+        nn @(mst.core/*state* :nn)]
+
+    (q/stroke-weight 3)
+
+    (q/stroke 0 0 255) ; lines
+    (doseq [edge (map #(concat (nth hood (first %)) (nth hood (second %))) nn)]
+      (show-line edge))
+    (q/stroke 255 0 0)
+    (doseq [edge (map #(concat (nth hood (first %)) (nth hood (second %))) mst)]
+      (show-line edge))
+
+    (q/stroke 0) ; points
+    (q/fill 0)
+    (doseq [point hood]
+      (show-point point))))
+
+(defn show []
+  (q/defsketch example
+    :title "MST"
+    :setup setup
+    :draw draw
+    :size [600 600]
+    :renderer :opengl))
